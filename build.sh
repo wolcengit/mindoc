4db808be38ebd56dfc44979c4452c4b0b7e29fbc
wolcen <wolcen@msn.com>
2018-07-04 07:46:10 +0800

    修正做法，使容器兼容master中定义的参数格式

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 MODE="$1"
 export MINDOC_VER="0.10.1-Z1"
-rm -rf mindoc
+rm -rf mindoc_linux_amd64
 #dep ensure -v
-go build -v -work -x -o mindoc -ldflags="-w -X github.com/lifei6671/mindoc/conf.VERSION=$MINDOC_VER -X 'github.com/lifei6671/mindoc/conf.BUILD_TIME=`date`' -X 'conf.GO_VERSION=`go version`'"
+go build -v -work -x -o mindoc_linux_amd64 -ldflags="-w -X github.com/lifei6671/mindoc/conf.VERSION=$MINDOC_VER -X 'github.com/lifei6671/mindoc/conf.BUILD_TIME=`date`' -X 'conf.GO_VERSION=`go version`'"
 if [ "$MODE"x = "push"x ]
 then
   cd mydocker/mindoc
--- mydocker/mindoc/run.sh
+++ mydocker/mindoc/run.sh
@@ -2,23 +2,22 @@
 TAG="$1"
 export SRC_HOME=../..
 # 准备
+mkdir zmindoc
+mkdir -p zmindoc/conf
+mkdir -p zmindoc/uploads
+cp -rf $SRC_HOME/conf/app.conf.example zmindoc/conf/
 cp -rf $SRC_HOME/lib zmindoc/
 cp -rf $SRC_HOME/static zmindoc/
 cp -rf $SRC_HOME/views zmindoc/
 cp -rf $SRC_HOME/LICENSE.md zmindoc/
 cp -rf $SRC_HOME/README.md zmindoc/
 cp -rf $SRC_HOME/favicon.ico zmindoc/
-cp -rf $SRC_HOME/mindoc zmindoc/
+cp -rf $SRC_HOME/mindoc_linux_amd64 zmindoc/
+cp -rf $SRC_HOME/start.sh zmindoc/
 # 创建
 docker build -t wolcen/mindoc:$TAG .
 # 清理
-rm -rf zmindoc/lib
-rm -rf zmindoc/static
-rm -rf zmindoc/views
-rm -rf zmindoc/LICENSE.md
-rm -rf zmindoc/README.md
-rm -rf zmindoc/favicon.ico
-rm -rf zmindoc/mindoc
+rm -rf zmindoc
 
 docker push wolcen/mindoc:$TAG
 
