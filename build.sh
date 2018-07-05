126e4234cb86c85e09a76c71711af797c33f1007
wolcen <wolcen@msn.com>
2018-07-05 21:45:11 +0800

    PushDocker:0.10.1-Z4

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MODE="$1"
-export MINDOC_VER="0.10.1-Z3"
+export MINDOC_VER="0.10.1-Z4"
 rm -rf mindoc_linux_amd64
 #dep ensure -v
 go build -v -work -x -o mindoc_linux_amd64 -ldflags="-w -X github.com/lifei6671/mindoc/conf.VERSION=$MINDOC_VER -X 'github.com/lifei6671/mindoc/conf.BUILD_TIME=`date`' -X 'conf.GO_VERSION=`go version`'"
