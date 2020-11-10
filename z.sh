a99661129e5e15091119a40b4b8f61eba14cad69
wolcen <wolcen@msn.com>
2020-11-10 13:46:32 +0800

    for compile and push docker

--- z.sh
+++ z.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 export TAG=Z3.22
-sudo docker build -t wolcen/mindoc:$TAG  .
+#sudo docker build -t wolcen/mindoc:$TAG  .
 sudo docker push wolcen/mindoc:$TAG
+sudo docker tag wolcen/mindoc:$TAG registry.cn-hangzhou.aliyuncs.com/wolcen/mindoc:$TAG
+sudo docker push registry.cn-hangzhou.aliyuncs.com/wolcen/mindoc:$TAG
